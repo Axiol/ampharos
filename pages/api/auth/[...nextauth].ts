406c0a3cf80c15ec9c768c81396028de33cf110c
Arnaud Delante <contact@arnaud.ninja>
2022-07-23 19:53:09 +0200

    feat: add basic authentication

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,12 @@
+import NextAuth from 'next-auth/next'
+import TwitterProvider from 'next-auth/providers/twitter'
+import GitHubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ? process.env.GITHUB_CLIENT_ID : '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ? process.env.GITHUB_CLIENT_SECRET : ''
+    })
+  ]
+})
