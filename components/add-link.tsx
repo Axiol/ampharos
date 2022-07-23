d1113c8ad0c9276b25c7de908985b0ba86476d8b
Arnaud Delante <contact@arnaud.ninja>
2022-07-24 00:33:59 +0200

    feat: add a Tailwind UI

--- components/add-link.tsx
+++ components/add-link.tsx
@@ -1,9 +1,9 @@
 import { FormEvent, useState } from "react"
 
 const AddLink: React.FC = () => {
-  const [url, setUrl] = useState<string>('https://google.be')
-  const [slug, setSlug] = useState<string>('test')
-  const [newLink, setNewLink] = useState<string | null>(null)
+  const [url, setUrl] = useState<string>('')
+  const [slug, setSlug] = useState<string>('')
+  const [newLink, setNewLink] = useState<string>('')
 
   const handleSubmit = async (e:FormEvent) => {
     e.preventDefault()
@@ -21,17 +21,42 @@ const AddLink: React.FC = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="url">URL</label>
-        <input type="url" id="url" name="url" value={url} onChange={e => setUrl(e.target.value )} required />
+      <form className="mt-6" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="url" className="block text-sm text-gray-800 dark:text-gray-200">URL</label>
+          <input type="url" id="url" name="url" value={url} onChange={e => setUrl(e.target.value )} required className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+        </div>
 
-        <label htmlFor="slug">Slug</label>
-        <input type="text" id="slug" name="slug" value={slug} onChange={e => setSlug(e.target.value )} required />
+        <div className="mt-4">
+          <label htmlFor="slug" className="block text-sm text-gray-800 dark:text-gray-200">Slug</label>
+          <input type="text" id="slug" name="slug" value={slug} onChange={e => setSlug(e.target.value )} required className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+        </div>
 
-        <button type="submit">Submit</button>
+        <div className="mt-6">
+          <button type="submit" className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+            Generate
+          </button>
+        </div>
       </form>
 
-      {newLink && <p>{newLink}</p>}
+      <div className="flex items-center justify-between mt-4">
+        <span className="w-2/5 border-b dark:border-gray-600 lg:w-1/5"></span>
+          <span className="text-xs text-center text-gray-500 uppercase dark:text-gray-400">Your link</span>
+        <span className="w-2/5 border-b dark:border-gray-400 lg:w-1/5"></span>
+      </div>
+
+      <div className="flex items-center mt-4">
+        <input value={newLink} readOnly className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+          
+
+        <button title="Copy to clipboard" className="p-2 ml-2 text-sm font-medium text-gray-500 transition-colors duration-200 transform bg-gray-300 rounded-md hover:bg-gray-200">
+          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708z"/>
+            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+          </svg>
+        </button>
+      </div>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
 import { useSession, signIn, signOut } from "next-auth/react"
 
 import AddLink from "@components/add-link"
@@ -14,18 +13,38 @@ const Home: NextPage = () => {
         <title>Ampharos - Link shortener</title>
       </Head>
 
-      <h1>Ampharos</h1>
-      {session ? (
-        <>
-          <button onClick={() => signOut()}>Sign out</button>
+      <div className='bg-gray-100 dark:bg-gray-900 h-screen pt-20'>
+        <div className="w-full max-w-sm p-6 m-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+          <h1 className="text-3xl font-semibold text-center text-gray-700 dark:text-white">Ampharos</h1>
 
-          <AddLink />
-        </>
-      ) : (
-        <>
-          <button onClick={() => signIn()}>Sign in</button>
-        </>
-      )}
+          {session ? (
+            <>
+              <AddLink />
+
+              <p className="mt-8 text-xs font-light text-center text-gray-400"> Wanne leave?&nbsp;
+                <button onClick={() => signOut()} className="font-medium text-gray-700 dark:text-gray-200 hover:underline">Log out</button>
+              </p>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center justify-between mt-4">
+                <span className="w-2/5 border-b dark:border-gray-600 lg:w-1/5"></span>
+                  <span className="text-xs text-center text-gray-500 uppercase dark:text-gray-400">Log in</span>
+                <span className="w-2/5 border-b dark:border-gray-400 lg:w-1/5"></span>
+              </div>
+
+              <div className="mt-4">
+                <button onClick={() => signIn('github')}className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                  Log in with GitHub
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="inline-block ml-2" viewBox="0 0 16 16">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                  </svg>
+                </button>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
     </>
   )
 }
